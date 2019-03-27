4d0c0828add814935532c7239d336c103b5814e3
Willem Olding <willemolding@gmail.com>
2019-03-27 14:29:43 +1000

    post dna created

--- dna-src/zomes/posts/code/src/post/mod.rs
+++ dna-src/zomes/posts/code/src/post/mod.rs
@@ -11,15 +11,36 @@ use hdk::{
     AGENT_ADDRESS,
 };
 
+const COMMENTER_LINK_TAG: &str = "commenter";
 
 #[derive(Serialize, Deserialize, Debug, Clone, DefaultJson)]
 pub struct Post {
-    pub announcement: bool,
     pub title: String,
     pub details: String,
     pub post_type: String,
     pub creator: Address,
+    pub announcement: bool,
     pub timestamp: String,
+}
+
+pub fn get_post(post_addr: Address) -> ZomeApiResult<Post> {
+    utils::get_as_type(post_addr)
+}
+
+pub fn create_post(title: String, details: String, post_type: String, announcement: bool, timestamp: String) -> ZomeApiResult<Address> {
+    hdk::commit_entry(
+        &Entry::App (
+            "post".into(),
+            Post {
+                title,
+                details,
+                post_type,
+                creator: AGENT_ADDRESS.to_string().into(),
+                announcement,
+                timestamp
+            }.into()
+        )
+    )
 }
 
 
@@ -39,9 +60,9 @@ pub fn def() -> ValidatingEntryType {
         },
 
         links: [
-            from!(
+            to!(
                 "%agent_id",
-                tag: "posts",
+                tag: COMMENTER_LINK_TAG,
 
                 validation_package: || {
                     hdk::ValidationPackageDefinition::Entry
