1282c7a4ec610201cbf5ca63a8364e64ad73c8aa
Willem Olding <willemolding@gmail.com>
2019-04-09 11:28:49 +1000

    adds full queries from Hylo around posts and comments

--- dna-src/test/queries.js
+++ dna-src/test/queries.js
@@ -151,3 +151,176 @@ query ($id: ID, $cursor: ID) {
     }
   }
 `
+
+
+module.exports.getPostQuery = `
+query ($id: ID) {  
+  post(id: $id) {
+    id  
+    announcement  
+    title  
+    details 
+    type  
+    creator {    
+      id    
+      name    
+      avatarUrl  
+    }  
+    createdAt  
+    updatedAt  
+    commenters(first: 3) {    
+      id    
+      name    
+      avatarUrl  
+    }  
+    commentersTotal  
+    comments(first: 10, order: "desc") {    
+      items {      
+        id      
+        text      
+        creator {        
+          id        
+          name        
+          avatarUrl      
+        }      
+        attachments {        
+          id        
+          url      
+        }      
+        createdAt    
+      }    
+      total    
+      hasMore  
+    }  
+    linkPreview {   
+     id    
+     title    
+     url    
+     imageUrl  
+   }  
+   votesTotal  
+   myVote  
+   communities {    
+     id    
+     name    
+     slug  
+   }  
+   attachments {    
+     id    
+     position    
+     type    
+     url  
+   }  
+   postMemberships {    
+     id    
+     pinned    
+     community {      
+       id    
+     }  
+   }  
+   topics {    
+     id    
+     name    
+     postsTotal    
+     followersTotal  
+   }  
+   members {    
+     total    
+     hasMore    
+     items {      
+       id      
+       name      
+       avatarUrl      
+       bio      
+       tagline      
+       location      
+       skills (first: 100) {        
+         items {          
+           id          
+           name        
+         }      
+       }    
+     }  
+   }  
+ }
+}
+`
+
+
+module.exports.getPostsQuery = `
+query (  $sortBy: String,  $offset: Int,  $search: String,  $filter: String,  $topic: ID,  $first: Int) {  
+  posts(  first: $first,  offset: $offset,  sortBy: $sortBy,  search: $search,  filter: $filter,  topic: $topic,  order: "desc") {  
+    hasMore  
+    items {      
+      id  
+      announcement  
+      title  
+      details  
+      type  
+      creator {    
+        id    
+        name    
+        avatarUrl  
+      }  
+      createdAt  
+      updatedAt  
+      commenters(first: 3) {    
+        id    
+        name    
+        avatarUrl  
+      }  
+      commentersTotal    
+      linkPreview {    
+        id    
+        title    
+        url    
+        imageUrl  
+      }  
+      votesTotal  
+      myVote  
+      communities {    
+        id    
+        name    
+        slug  
+      }  
+      attachments {    
+        id    
+        position    
+        type    
+        url  
+      }  
+      postMemberships {    
+        id    
+        pinned    
+        community {      
+          id    
+        }  
+      }  
+      topics {    
+        id    
+        name    
+        postsTotal    
+        followersTotal  
+      }  
+      members {    
+        total    
+        hasMore    
+        items {      
+          id      
+          name      
+          avatarUrl      
+          bio      
+          tagline      
+          location      
+          skills (first: 100) {        
+            items {          
+              id          
+              name        
+            }      
+          }    
+        }  
+      }  
+    }
+  }
+}
+`
