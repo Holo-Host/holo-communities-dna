06686689251e5c7bc175f4a7907bbc97490608eb
Willem Olding <willemolding@gmail.com>
2019-12-19 14:04:44 +1100

    reenable other tests

--- test/index.js
+++ test/index.js
@@ -30,12 +30,12 @@ const orchestrator = new Orchestrator({
   }
 })
 
-// require('./single_agent/communities')(orchestrator.registerScenario)
-// require('./single_agent/posts')(orchestrator.registerScenario)
-// require('./single_agent/comments')(orchestrator.registerScenario)
-// require('./single_agent/threads')(orchestrator.registerScenario)
-// require('./single_agent/messages')(orchestrator.registerScenario)
-// require('./single_agent/people')(orchestrator.registerScenario)
+require('./single_agent/communities')(orchestrator.registerScenario)
+require('./single_agent/posts')(orchestrator.registerScenario)
+require('./single_agent/comments')(orchestrator.registerScenario)
+require('./single_agent/threads')(orchestrator.registerScenario)
+require('./single_agent/messages')(orchestrator.registerScenario)
+require('./single_agent/people')(orchestrator.registerScenario)
 
 require('./multi_agent/posts')(orchestrator.registerScenario)
 
