465f09084fa22136bcae4da89272177db34d7976
Loren Johnson <lorenjohnson@gmail.com>
2020-03-03 16:06:30 -0800

    Update holonix version, and to hdk v0.0.44-alpha3

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/fc160c13bd2f88409d09866ea59945b9579dd23a.tar.gz";
-  sha256 = "09wsb9n62rxgr4pqz2z1098xcj8fqwxnq0iv0lhprj15vbkxpgih";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/f22c34ffde49c2c4ddc15c3f8d62c17f133694e7.tar.gz";
+  sha256 = "0pa9y9yi4d2n351jai4g9wqpj0dpqvrn6mhijmbxgwgk87jb1mwq";
 })
